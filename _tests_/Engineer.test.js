a21b84dd36abe3eaac88ce5f9b675b829bc2b042
Ben Koenig <70115233+taqft@users.noreply.github.com>
2023-09-07 12:11:59 -0600

    Update Engineer.test.js

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -2,18 +2,18 @@
 const Engineer = require('../lib/engineer');
 describe('Engineer', () => {
     it('Should generate a new Engineer object with the given arguments', () => {
-        const engineer = new Engineer('Ben Koenig', 12345678, 'test@tester', 'theresaqueryforthat');
+        const engineer = new Engineer('Ben Koenig', 12345678, 'test@tester', 'taqft');
         expect(engineer.name).toMatch('Ben Koenig');
         expect(engineer.id).toEqual(expect.any(Number));
         expect(engineer.email).toMatch('test@tester');
-        expect(engineer.github).toMatch('theresaqueryforthat');
+        expect(engineer.github).toMatch('taqft');
     });
     it("Should return the engineer's github handle via the getGithub() method", () => {
-        const engineer = new Engineer('Ben Koenig', 12345678, 'test@tester', 'theresaqueryforthat');
-        expect(engineer.getGithub()).toMatch('theresaqueryforthat');
+        const engineer = new Engineer('Ben Koenig', 12345678, 'test@tester', 'taqft');
+        expect(engineer.getGithub()).toMatch('taqft');
     });
     it('Should get the role of Engineer with the getRole() method', () => {
-        const engineer = new Engineer('Ben Koenig', 12345678, 'test@tester', 'theresaqueryforthat');
+        const engineer = new Engineer('Ben Koenig', 12345678, 'test@tester', 'taqft');
         expect(engineer.getRole()).toMatch('Engineer');
     });
 });
